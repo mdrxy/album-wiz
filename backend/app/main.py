3141bcd24d4b611fb610fccbb1a48480819f9800
Tom Han <103209542+tom21100227@users.noreply.github.com>
2024-12-03 17:02:17 -0500

    Added a backend test website, stubbed the query API

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,12 @@ import os
 from typing import AsyncGenerator
 from fastapi import FastAPI, HTTPException
 import asyncpg
-import query
-import normalize
 from fastapi import UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import io
+
+from . import normalize, query
 
 DATABASE_URL = os.getenv("DATABASE_URL") # From docker-compose.yml
 
@@ -26,6 +27,20 @@ async def lifespan(application: FastAPI) -> AsyncGenerator:
     await application.state.pool.close()
 
 app = FastAPI(lifespan=lifespan)
+
+# Configure CORS
+origins = [
+    "http://localhost:3000",
+    # Add other origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # API endpoint to fetch all artists
 @app.get("/api/artists")
@@ -48,11 +63,30 @@ async def vectorize(file: UploadFile = File(...)):
     """
     Endpoint to receive an image file and convert it to a PIL image.
     """
+    print("I am here!!")
     try:
+        print("Reading file contents...")
         contents = await file.read()
+        print("File contents read successfully.")
+        
+        print("Opening image...")
         image = Image.open(io.BytesIO(contents))
-        vector = query.vectorize(image)
+        print("Image opened successfully.")
+        
+        print("Cropping image to square...")
+        square_image = normalize.crop_to_square(image)
+        print("Image cropped to square successfully.")
+        
+        print("Vectorizing image...")
+        vector = query.vectorize(square_image)
+        print("Image vectorized successfully.")
+        
+        print(f"Vector: {vector}")
+        
+        # TODO: Stop serialized with the stupid fucking python list.
         return {"message": "Image successfully converted", 
-                "vector": vector}
-    except Exception as e:
+                "vector": vector.tolist()}
+    except HTTPException as e:
         raise HTTPException(status_code=400, detail=str(e)) from e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
--- backend/app/query.py
+++ backend/app/query.py
@@ -1,1 +1,9 @@
-import torch
+from PIL import Image
+import numpy as np
+from functools import lru_cache
+
+# @lru_cache(maxsize=128)
+def vectorize(image: Image): 
+    pixels = np.array(image)
+    vector = pixels[:3, :, 1].flatten()
+    return vector
