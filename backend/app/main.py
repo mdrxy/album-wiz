4fb42b1bc0402d9861110803b3dc64304c14d590
Mason Daugherty <mdaugher@bowdoin.edu>
2024-12-03 19:52:43 -0500

    Use absolute import path, add generalized table query

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,21 @@
 import os
-from typing import AsyncGenerator
-from fastapi import FastAPI, HTTPException
-import asyncpg
-from fastapi import UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
-from PIL import Image
 import io
+from typing import AsyncGenerator
+from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import asyncpg
+from PIL import Image
 
-from . import normalize, query
+from app import normalize, query
 
-DATABASE_URL = os.getenv("DATABASE_URL") # From docker-compose.yml
+DATABASE_URL = os.getenv("DATABASE_URL")  # From docker-compose.yml
+
 
 async def lifespan(application: FastAPI) -> AsyncGenerator:
     """
     Database connection pool setup and teardown.
 
-    A connection pool is a cache of database connections maintained so that the 
+    A connection pool is a cache of database connections maintained so that the
     connections can be reused when needed.
 
     Function creates a connection pool when the application starts and closes
@@ -26,11 +26,12 @@ async def lifespan(application: FastAPI) -> AsyncGenerator:
     yield  # Yield control to the application
     await application.state.pool.close()
 
+
 app = FastAPI(lifespan=lifespan)
 
 # Configure CORS
 origins = [
-    "http://localhost:3000",
+    "http://localhost:3000",  # React frontend
     # Add other origins as needed
 ]
 
@@ -42,18 +43,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# API endpoint to fetch all artists
-@app.get("/api/artists")
-async def get_artists():
+
+@app.get("/api/{table_name}")
+async def get_table_data(table_name: str):
     """
-    Retrieve all artists from the database.
+    Retrieve all data from the specified table in the database.
     """
-    query = "SELECT * FROM artists;"
+    sql_query = f"SELECT * FROM {table_name};"
     try:
         async with app.state.pool.acquire() as connection:
-            rows = await connection.fetch(query)
-            artists = [dict(row) for row in rows]
-            return artists
+            rows = await connection.fetch(sql_query)
+            data = [dict(row) for row in rows]
+            return data
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e)) from e
 
@@ -68,24 +69,23 @@ async def vectorize(file: UploadFile = File(...)):
         print("Reading file contents...")
         contents = await file.read()
         print("File contents read successfully.")
-        
+
         print("Opening image...")
         image = Image.open(io.BytesIO(contents))
         print("Image opened successfully.")
-        
+
         print("Cropping image to square...")
         square_image = normalize.crop_to_square(image)
         print("Image cropped to square successfully.")
-        
+
         print("Vectorizing image...")
         vector = query.vectorize(square_image)
         print("Image vectorized successfully.")
-        
+
         print(f"Vector: {vector}")
-        
+
         # TODO: Stop serialized with the stupid fucking python list.
-        return {"message": "Image successfully converted", 
-                "vector": vector.tolist()}
+        return {"message": "Image successfully converted", "vector": vector.tolist()}
     except HTTPException as e:
         raise HTTPException(status_code=400, detail=str(e)) from e
     except Exception as e:
