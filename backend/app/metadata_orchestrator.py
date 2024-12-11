cc7ef19e9ba8d4ddf6f8b7c957fe86b9afd934f2
Mason Daugherty <mdaugher@bowdoin.edu>
2024-12-11 01:26:50 -0500

    Refactor metadata collection to allow specifying collector source and improve error handling

--- backend/app/metadata_orchestrator.py
+++ backend/app/metadata_orchestrator.py
@@ -28,14 +28,59 @@ class MetadataOrchestrator:
             MusicBrainzCollector("musicbrainz"),
         ]
 
-    async def collect_metadata(self, query: str) -> dict:
+    async def collect_metadata(self, source: str, query: str) -> dict:
+        """
+        Collect metadata for a given album or artist query using a specific collector.
+
+        Args:
+        - source (str): The name of the collector to use.
+        - query (str): The query to search for, in the format: "{artist} - {album}".
+
+        Returns:
+        - dict: A dictionary containing the metadata from the specified collector.
+        """
+        metadata = {}
+        collector = next((c for c in self.collectors if c.get_name() == source), None)
+        if collector is None:
+            self.logger.error("Collector %s not found", source)
+            return {"error": f"Collector {source} not found"}
+
+        try:
+            self.logger.info(
+                "Collecting metadata using %s", collector.__class__.__name__
+            )
+            source_metadata = await collector.fetch_metadata(query)
+            if "error" not in source_metadata:
+                metadata[collector.get_name()] = source_metadata
+                self.logger.info(
+                    "Successfully collected metadata from %s",
+                    collector.__class__.__name__,
+                )
+            else:
+                self.logger.warning(
+                    "Error in metadata from %s: %s",
+                    collector.__class__.__name__,
+                    source_metadata["error"],
+                )
+                metadata["error"] = source_metadata["error"]
+        except (ConnectionError, TimeoutError, ValueError) as e:
+            error_message = str(e)
+            metadata["error"] = error_message
+            self.logger.error(
+                "Failed to collect metadata from %s: %s",
+                collector.__class__.__name__,
+                error_message,
+            )
+
+        return metadata
+
+    async def collect_all_metadata(self, query: str) -> dict:
         """
         Collect metadata for a given album or artist query using all available
         collectors.
 
         Args:
-        - query (str): The query to search for. This should probably be in the
-            format: "{artist} - {album}".
+        - query (str): The query to search for, in the format: "{artist} - {album}".
 
         Returns:
         - dict: A dictionary containing the metadata from all available
