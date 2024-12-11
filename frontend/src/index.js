c10211dd2f4f5f903aa672b7857e00e3577ab9a6
Mason Daugherty <mdaugher@bowdoin.edu>
2024-12-11 05:26:01 -0500

    Add navigation and routing to the App component with Metadata Manager

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css'; // Importing global styles
-import App from "./App"; // Importing the main App component
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-// Create a root element for React to render into
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// Render the main App component
 root.render(
   <React.StrictMode>
-    {/* StrictMode helps identify potential problems in the application by enabling additional checks during development */}
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
