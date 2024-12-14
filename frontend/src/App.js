874046fab610d09cc330483bfa4a5e69a371d090
Mason Daugherty <mdaugher@bowdoin.edu>
2024-12-14 17:06:28 -0500

    Hot reloading fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,14 +8,16 @@ const App = () => {
       {/* A simple Navbar */}
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">album-wiz</Link>
-          <button 
-            className="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarSupportedContent" 
-            aria-controls="navbarSupportedContent" 
-            aria-expanded="false" 
+          <Link className="navbar-brand" to="/">
+            album-wiz
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
@@ -23,7 +25,9 @@ const App = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link" to="/metadata">Metadata Manager</Link>
+                <Link className="nav-link" to="/metadata">
+                  Metadata Manager
+                </Link>
               </li>
             </ul>
           </div>
